06ccc27bf33fc9ebba3511a03f821b294c6ef9d4
Bipinjot <bips09@yahoo.com>
2023-06-08 00:05:45 -0400

    Adding exception handling for get results but userId

--- src/main/java/com/example/demo/result/ResultController.java
+++ src/main/java/com/example/demo/result/ResultController.java
@@ -1,8 +1,12 @@
 package com.example.demo.result;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,13 +19,38 @@ public class ResultController {
     }
 
     @RequestMapping("/results/user/{userId}")
-    public List<Result> getAllResultsByUserId(@PathVariable int userId){
-        return resultService.getAllResultsByUserId(userId);
+    public ResponseEntity<List<Result>> getAllResultsByUserId(@PathVariable int userId){
+        try {
+            List<Result> result = resultService.getAllResultsByUserId(userId);
+            if (result.size() <= 0)
+            {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User Result not found");
+            }
+            return ResponseEntity.ok(result);
+        }
+        catch (Exception e) {
+            // Handle the exception and return an appropriate response
+            // You can customize the error message based on the exception type
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body(new ArrayList<>());
+        }
     }
 
     @RequestMapping("/results/course/{courseId}")
-    public List<Result> getAllResultsByCourseId(@PathVariable int courseId){
-        return resultService.getAllResultsByCourseId(courseId);
+    public ResponseEntity<List<Result>> getAllResultsByCourseId(@PathVariable int courseId){
+        try{
+            List<Result> result = resultService.getAllResultsByCourseId(courseId);
+            if (result.size() <= 0)
+            {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User Result not found by given course id");
+            }
+            return ResponseEntity.ok(result);
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body(new ArrayList<>());
+        }
     }
 
     @RequestMapping("/results/{id}")
